5a539c661511af2aff86ca710ee9ad7c26559cd6
jagdish-gts <jagdish@gtsinfosoft.com>
2024-01-28 16:52:39 +0530

    first code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,24 @@
 
 export const environment = {
   production: false,
+  //jagdish fire
+  // firebaseConfig : {
+  //   apiKey: "AIzaSyDuAMRs1AiISSO0UK5uc5NqE8OVXNSRgoY",
+  //   authDomain: "casedatabase-a68b5.firebaseapp.com",
+  //   projectId: "casedatabase-a68b5",
+  //   storageBucket: "casedatabase-a68b5.appspot.com",
+  //   messagingSenderId: "763349878909",
+  //   appId: "1:763349878909:web:dd50c35d263a78cfb40914"
+  // }
+  // bharat fire
   firebaseConfig : {
-    apiKey: "AIzaSyDuAMRs1AiISSO0UK5uc5NqE8OVXNSRgoY",
-    authDomain: "casedatabase-a68b5.firebaseapp.com",
-    projectId: "casedatabase-a68b5",
-    storageBucket: "casedatabase-a68b5.appspot.com",
-    messagingSenderId: "763349878909",
-    appId: "1:763349878909:web:dd50c35d263a78cfb40914"
+    apiKey: "AIzaSyAy-Jrnrv-B0o0L5byGm3mOegIeI-SCDhE",
+    authDomain: "casedatabase-38b02.firebaseapp.com",
+    projectId: "casedatabase-38b02",
+    storageBucket: "casedatabase-38b02.appspot.com",
+    messagingSenderId: "10004392777",
+    appId: "1:10004392777:web:6fb2db91b48a2924aec898",
+    measurementId: "G-T16SLF6Q91"
   }
 };
 
